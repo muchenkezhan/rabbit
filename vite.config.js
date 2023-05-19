7ad1803fdd0d1eeb141b03552959187109ddfb38
‘858896214@qq.com’ <858896214@qq.com>
2023-05-19 19:18:19 +0800

    elementPlus自动按需引入配置

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,25 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// 配置elementui plus 按需导入
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+// 配置elementui
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    // 配置elementui plus 按需导入
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+    // 配置elementui plus 按需导入
+  ],
   resolve: {
     // 实际路径转换  @  ->  src
     alias: {
