602bc12c69266cbb879fc28c1d39ecff54653f64
‘858896214@qq.com’ <858896214@qq.com>
2023-05-19 21:46:52 +0800

    静态资源创建

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,39 @@
+// createRouter: 插件路由实例
+// createWebHistory：创建history模式的路由
+
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Login from "@/views/Login/index.vue";
+import Layout from "@/views/Layout/index.vue";
+import Home from "@/views/Home/index.vue";
+import Category from "@/views/Home/index.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  // 配置 path 和component对应关系位置
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      component: Layout,
+      name: 'layout',
+      children: [
+        {
+          // 访问/的时候为空的路径默认渲染
+          path: '',
+          component: Home,
+
+        },
+        {
+          path: 'category',
+          component: Category,
+
+        }
+      ]
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+      path: '/login',
+      component: Login,
+      name: 'login'
+    },
   ]
 })
 
