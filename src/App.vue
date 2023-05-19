d24d7aeb352d1578ae2fb5c69b5bd9ab91bedc15
‘858896214@qq.com’ <858896214@qq.com>
2023-05-19 22:05:42 +0800

    scss文件的自动导入

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,11 @@
 <template>
   <!-- 准备路由显示区域 -->
     <RouterView></RouterView>
+    <div class="test">tese scss</div>
 </template>
 
-<style scoped>
-
+<style scoped lang="scss">
+.test{
+  color: $warnColor;
+}
 </style>
