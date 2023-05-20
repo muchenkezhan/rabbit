1209aaf5576e04e8f5a7bcf527eb0a00265cd6a3
‘858896214@qq.com’ <858896214@qq.com>
2023-05-20 14:44:38 +0800

    home-product-区域组列表渲染

--- src/apis/home.js
+++ src/apis/home.js
@@ -5,3 +5,9 @@ export const getBannerAPI = ()=>httpInstance({url:"/home/banner",method:'get'});
 
 // 获取-新鲜好物
 export const findNewAPI = ()=>httpInstance({url:"/home/new",method:'get'});
+
+// 获取-人气推荐
+export const getGoodsAPI = ()=>httpInstance({url:"/home/goods",method:'get'});
+
+// 获取所有商品模块
+export const getHotAPI = ()=>httpInstance({url:"/home/hot",method:'get'});
