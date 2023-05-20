f13f4b379c19ff222cd2ff316a323aba72c42820
‘858896214@qq.com’ <858896214@qq.com>
2023-05-20 15:20:38 +0800

    home-product-goodsItem组件的封装

--- src/views/Home/components/HomeProduct.vue
+++ src/views/Home/components/HomeProduct.vue
@@ -1,5 +1,7 @@
 <script setup>
 import HomePanel from './HomePanel/index.vue'
+// 导入封装好的卡片组件
+import GoodsItem from "@/views/Home/components/GoodsItem/index.vue";
 // 导入接口
 import { getGoodsAPI } from "@/apis/home";
 import { onMounted, ref } from "vue";
@@ -32,14 +34,7 @@ onMounted(() => {
                     </strong>
                 </RouterLink>
                 <ul class="goods-list">
-                    <li v-for="good in cate.goods" :key="good.id">
-                        <RouterLink to="/" class="goods-item">
-                            <img v-img-lazy="good.picture" alt="" />
-                            <p class="name ellipsis">{{ good.name }}</p>
-                            <p class="desc ellipsis">{{ good.desc }}</p>
-                            <p class="price">&yen;{{ good.price }}</p>
-                        </RouterLink>
-                    </li>
+                    <GoodsItem :good="good" ></GoodsItem>
                 </ul>
             </div>
         </HomePanel>
@@ -134,41 +129,7 @@ onMounted(() => {
             }
         }
 
-        .goods-item {
-            display: block;
-            width: 220px;
-            padding: 20px 30px;
-            text-align: center;
-            transition: all .5s;
 
-            &:hover {
-                transform: translate3d(0, -3px, 0);
-                box-shadow: 0 3px 8px rgb(0 0 0 / 20%);
-            }
-
-            img {
-                width: 160px;
-                height: 160px;
-            }
-
-            p {
-                padding-top: 10px;
-            }
-
-            .name {
-                font-size: 16px;
-            }
-
-            .desc {
-                color: #999;
-                height: 29px;
-            }
-
-            .price {
-                color: $priceColor;
-                font-size: 20px;
-            }
-        }
     }
 }
 </style>
