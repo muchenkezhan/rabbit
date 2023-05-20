e44d8c60918f84417b4a2dda2417854f69641950
‘858896214@qq.com’ <858896214@qq.com>
2023-05-20 12:55:51 +0800

    封装home页面板组件的

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -4,6 +4,7 @@ import HomeBanner from './components/HomeBanner.vue'
 import HomeNew from './components/HomeNew.vue'
 import HomeHot from './components/HomeHot.vue'
 import homeProduct from './components/HomeProduct.vue'
+import HomePanel from "./components/HomePanel/index.vue"
 </script>
 
 <template>
@@ -14,4 +15,12 @@ import homeProduct from './components/HomeProduct.vue'
   <HomeNew />
   <HomeHot />
   <homeProduct />
+  <HomePanel title="新鲜好物" subTitle="新鲜好物好  好多商品">
+    <div>
+      我是插槽部分
+    </div>
+  </HomePanel>
+  <HomePanel title="人气推荐" subTitle="新鲜好物好  好多商品">
+    
+  </HomePanel>
 </template>
