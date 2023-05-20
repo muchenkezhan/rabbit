1e64d2547165f285033ae9fa93cdffbc1d79e1c4
‘858896214@qq.com’ <858896214@qq.com>
2023-05-20 13:12:15 +0800

    home-新鲜好物模块业务实现

--- src/apis/home.js
+++ src/apis/home.js
@@ -1,3 +1,7 @@
 import httpInstance from "@/utils/http.js";
+
 // 获取-轮播图数据
 export const getBannerAPI = ()=>httpInstance({url:"/home/banner",method:'get'});
+
+// 获取-新鲜好物
+export const findNewAPI = ()=>httpInstance({url:"/home/new",method:'get'});
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -4,7 +4,6 @@ import HomeBanner from './components/HomeBanner.vue'
 import HomeNew from './components/HomeNew.vue'
 import HomeHot from './components/HomeHot.vue'
 import homeProduct from './components/HomeProduct.vue'
-import HomePanel from "./components/HomePanel/index.vue"
 </script>
 
 <template>
@@ -15,12 +14,4 @@ import HomePanel from "./components/HomePanel/index.vue"
   <HomeNew />
   <HomeHot />
   <homeProduct />
-  <HomePanel title="新鲜好物" subTitle="新鲜好物好  好多商品">
-    <div>
-      我是插槽部分
-    </div>
-  </HomePanel>
-  <HomePanel title="人气推荐" subTitle="新鲜好物好  好多商品">
-    
-  </HomePanel>
 </template>
