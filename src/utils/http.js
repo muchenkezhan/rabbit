4c755274958c72563cce4187141a5571d65e98a6
‘858896214@qq.com’ <858896214@qq.com>
2023-05-22 15:51:42 +0800

    请求拦截器中携带token

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,5 +1,6 @@
 // axios基础封装
 import axios from 'axios'
+import { useUserStore } from "@/stores/user";
 
 // 创建axios实例
 const httpInstance = axios.create({
@@ -9,6 +10,14 @@ const httpInstance = axios.create({
 
 // axios请求拦截器
 httpInstance.interceptors.request.use(config => {
+  // 1.从pinia获取到存储到本地的token
+  // 得到实例
+  const userStore =useUserStore()
+  // 2.获取token
+  if(userStore.usereInfo.token){
+    // 根据后端要求拼接token
+    config.headers.Authorization = `Beaer ${userStore.usereInfo.token}`
+  }
     return config
   }, e => Promise.reject(e))
   
