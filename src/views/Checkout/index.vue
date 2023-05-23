eafbb355208faea78a8caf545f86fadcd1ccd5f2
‘858896214@qq.com’ <858896214@qq.com>
2023-05-23 12:20:17 +0800

    切换地址的激活交互

--- src/views/Checkout/index.vue
+++ src/views/Checkout/index.vue
@@ -21,9 +21,19 @@ const getCheckInfo = async () => {
 onMounted(() => {
     getCheckInfo()
 })
-
-
+// 切换地址
+const activeAddress = ref({})
+const switchAddress = (item) => {
+    activeAddress.value = item
+    console.log(activeAddress);
+}
 const showDialog = ref(false)
+
+const confirm = () => {
+    curAddress.value = activeAddress.value,
+    showDialog.value = false,
+    activeAddress.value = {}
+}
 
 </script>
 
@@ -127,7 +137,8 @@ const showDialog = ref(false)
     <!-- 切换地址 -->
     <el-dialog title="切换收货地址" width="30%" center v-model="showDialog">
         <div class="addressWrapper">
-            <div class="text item" v-for="item in checkInfo.userAddresses" :key="item.id">
+            <div class="text item" :class="{ 'active': item.id == activeAddress.id }"
+                v-for="(item, index) in checkInfo.userAddresses" :key="item.id" @click="switchAddress(item)">
                 <ul>
                     <li><span>收<i />货<i />人：</span>{{ item.receiver }} </li>
                     <li><span>联系方式：</span>{{ item.contact }}</li>
@@ -137,8 +148,8 @@ const showDialog = ref(false)
         </div>
         <template #footer>
             <span class="dialog-footer">
-                <el-button>取消</el-button>
-                <el-button type="primary">确定</el-button>
+                <el-button @click="showDialog = false">取消</el-button>
+                <el-button type="primary" @click="confirm">确定</el-button>
             </span>
         </template>
     </el-dialog>
